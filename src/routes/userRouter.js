49d6d3812828fb83aef0adb9c3a1502257dc0e84
pavanshekar <shekarpavan97@gmail.com>
2021-11-25 16:16:06 +0530

    Added jwt and hashing

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,20 +1,39 @@
+require('dotenv').config()
+
 const express = require('express');
 const router = express.Router();
 
 const userService = require('../service/userService');
 const User = require('../model/User')
+const validator = require('../utilities/Validator');
+
+const jwt = require('jsonwebtoken')
+
+const bcrypt = require('bcrypt')
 
 //To login 
 router.post('/login', function(req, res, next) {
     let credentials = req.body.credentials;
     let password = req.body.password;
+
+    const user = { credentials: credentials, password: password}
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
     userService.checkUser(credentials, password).then(result => {
-        res.json(result);
+        // res.json(result);
+        res.json({result, accessToken: accessToken, status: 200})
     }).catch(err => next(err));
 })
 
 //to register 
-router.post('/register', function (req, res, next) {
+router.post('/register', async function (req, res, next) {
+    validator.validateUserName(req.body.name);
+    validator.validateEmailId(req.body.emailId);
+    validator.validateContactNo(req.body.contactNo);
+    validator.validatePassword(req.body.password);
+    let salt = await bcrypt.genSalt(15)
+    let hash = await bcrypt.hash(req.body.password, salt)
+    req.body.password = hash
     const user = new User(req.body);
     userService.addDetails(user).then(result => {
         if (result != null)
@@ -34,5 +53,17 @@ router.put('/resetPassword', function (req, res, next) {
         next(err);
     })
 })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authoriation']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token ==null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 module.exports = router;
--- src/service/userService.js
+++ src/service/userService.js
@@ -2,6 +2,8 @@ const User = require('../model/User');
 const userdb = require('../model/userModel');
 const validator = require('../utilities/Validator');
 
+const bcrypt = require('bcrypt')
+
 let UserService = {}
 
 //To check whether the user registered if so, then allow him/her to login
@@ -25,13 +27,16 @@ UserService.checkUser = (credentials, password) => {
             throw err;
         } else {
             if (userData.emailId === emailId || userData.contactNo == contactNo) {
-                if (userData.password === password)
-                    return userData;
-                else {
-                    let err = new Error("Password is Incorrect");
-                    err.status = 404;
-                    throw err;
-                }
+                // if (userData.password === password)
+                bcrypt.compare(password, userData.password, (err, data) => {
+                    if (data) {
+                        return userData;
+                    } else {
+                        err = new Error("Password is Incorrect");
+                        err.status = 404;
+                        throw err;
+                    }
+                });
             }
             else {
                 let err = new Error("Authentication failed");
@@ -44,10 +49,6 @@ UserService.checkUser = (credentials, password) => {
 
 //Tocheckwhileregisteringthatwhetherthelogincredential already exist or not. If not allow him/her to register
 UserService.addDetails = (UserObj) => {
-    validator.validateUserName(UserObj.name);
-    validator.validateEmailId(UserObj.emailId);
-    validator.validateContactNo(UserObj.contactNo);
-    validator.validatePassword(UserObj.password);
     return userdb.findUser(UserObj.contactNo, UserObj.emailld).then(object => {
         {
             if (object != null) {
